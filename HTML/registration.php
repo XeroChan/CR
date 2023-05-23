4a21900a23ca66cc3e7a44ca1cb6855eeaab82d5
XeroChan <maciej2013.mp@gmail.com>
2023-05-24 01:10:31 +0200

    Checking if user is in database

--- HTML/registration.php
+++ HTML/registration.php
@@ -8,69 +8,59 @@ session_start()
     <script>
         window.onload = function() {
             <?php
-            if (isset($_SESSION['successR'])) {
-            $successR = $_SESSION['successR'];
-            unset($_SESSION['successR']);
-            if ($successR) {
-            ?>
-            alert("Rejestracja przebiegła pomyślnie.");
-            <?php
-            } else {
-            ?>
-            alert("Wystąpił problem z rejestracją.");
-            <?php
-            }
-            }
+                if (isset($_SESSION['message'])) {
+                    $message = $_SESSION['message'];
+                    unset($_SESSION['message']);
+                }
             ?>
         }
     </script>
+    <title>Rejestracja</title>
 </head>
 <body>
-<section class="navBar">
-    <section id="logo" onclick="location.href='index.php'">Euroautka</section>
+    <section class="navBar">
+        <section id="logo" onclick="location.href='index.php'">Euroautka</section>
 
-    <?php
-    if(isset($_SESSION['username'])){
-        $usern = $_SESSION['username'];
-        echo 'Witaj '.$usern."! ";
-        echo '<form method="GET" id="logoutForm" action="logout.php"'.'"><button type="submit" id="logoutButton">Wyloguj się</button></form>';
-    } else{
-        echo '<form method="POST" action="login.php">';
-        echo '<button type="submit" id="loginButton">Zaloguj się</button>';
-        echo '</form>';
-    }
-    ?>
-</section>
+        <?php
+        if(isset($_SESSION['username'])){
+            $usern = $_SESSION['username'];
+            echo 'Witaj '.$usern."! ";
+            echo '<form method="GET" id="logoutForm" action="logout.php"'.'"><button type="submit" id="logoutButton">Wyloguj się</button></form>';
+        } else{
+            echo '<form method="POST" action="login.php">';
+            echo '<button type="submit" id="loginButton">Zaloguj się</button>';
+            echo '</form>';
+        }
+        ?>
+    </section>
+    <div class="registration-container">
+        <h1>Zarejestruj się</h1>
+        <?php if (!empty($message)) { ?>
+            <p><?php echo $message; ?></p>
+        <?php } ?>
+        <form method="POST" action="../PHP/registerUser.php">
+            <label for="name">Imie:</label>
+            <input type="text" id="name" name="name" required>
 
-<div class="registration-container">
-    <h1>Zarejestruj się</h1>
-    <?php if (!empty($message)) { ?>
-        <p><?php echo $message; ?></p>
-    <?php } ?>
-    <form method="POST" action="../PHP/registerUser.php">
-        <label for="name">Imie:</label>
-        <input type="text" id="name" name="name" required>
+            <label for="sname">Nazwisko:</label>
+            <input type="text" id="sname" name="sname" required>
 
-        <label for="sname">Nazwisko:</label>
-        <input type="text" id="sname" name="sname" required>
+            <label for="username">Nazwa użytkownika:</label>
+            <input type="text" id="username" name="username" required>
 
-        <label for="username">Nazwa użytkownika:</label>
-        <input type="text" id="username" name="username" required>
+            <label for="email">Adres e-mail:</label>
+            <input type="email" id="email" name="email" required>
 
-        <label for="email">Adres e-mail:</label>
-        <input type="email" id="email" name="email" required>
+            <label for="phoneN">Nr Tel:</label>
+            <input type="tel" id="phoneN" name="phoneN" required>
 
-        <label for="phoneN">Nr Tel:</label>
-        <input type="tel" id="phoneN" name="phoneN" required>
+            <label for="password">Hasło:</label>
+            <input type="password" id="password" name="password" required>
 
-        <label for="password">Hasło:</label>
-        <input type="password" id="password" name="password" required>
-
-        <div class="button-container">
-            <button type="submit">Zarejestruj się</button>
-        </div>
-    </form>
-</div>
-
+            <div class="button-container">
+                <button type="submit">Zarejestruj się</button>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
--- PHP/registerUser.php
+++ PHP/registerUser.php
@@ -6,29 +6,36 @@ if (isset($host) && isset($user) && isset($pass) && isset($database)) {
     if ($connect->connect_errno != 0) {
         echo "Error: " . $connect->connect_errno . " Description: " . $connect->connect_error;
     } else {
+
         $login = $_POST['username'];
         $password = $_POST['password'];
 
-        $name = $_POST['name'];
-        $sname = $_POST['sname'];
-        $email = $_POST['email'];
-        $tel = $_POST['phoneN'];
+        $query = "SELECT * FROM tbl_users WHERE Username='$login' AND Password='$password'";
+        if ($result = @$connect->query($query)) {
+            $users = $result->num_rows;
+            if ($users > 0) {
+                $_SESSION['message'] = "Użytkownik o takiej nazwie jest już zarejestrowany!";
+                header('Location: ../HTML/registration.php');
+            } else {
+                $name = $_POST['name'];
+                $sname = $_POST['sname'];
+                $email = $_POST['email'];
+                $tel = $_POST['phoneN'];
 
-        $query = "INSERT INTO tbl_users (Username, Password, Name, Surname, Email, Phone) VALUES (?, ?, ?, ?, ?, ?)";
-        $stmt = $connect->prepare($query);
-        $stmt->bind_param("ssssss", $login, $password, $name, $sname, $email, $tel);
-        $stmt->execute();
-        if ($stmt->affected_rows > 0) {
-            $_SESSION['message'] = "Registration successful. Please login.";
-            $_SESSION['successR'] = true;
-            header('Location: ../HTML/registration.php');
-        } else {
-            $_SESSION['message'] = "Registration failed.";
-            $_SESSION['successR'] = false;
-            header('Location: ../HTML/registration.php');
+                $query = "INSERT INTO tbl_users (Username, Password, Name, Surname, Email, Phone) VALUES (?, ?, ?, ?, ?, ?)";
+                $stmt = $connect->prepare($query);
+                $stmt->bind_param("ssssss", $login, $password, $name, $sname, $email, $tel);
+                $stmt->execute();
+                if ($stmt->affected_rows > 0) {
+                    $_SESSION['message'] = "Rejestracja się powiodła!";
+                    header('Location: ../HTML/registration.php');
+                } else {
+                    $_SESSION['message'] = "Błąd rejestracji!.";
+                    header('Location: ../HTML/registration.php');
+                }
+            }
         }
-    }
 
-    $stmt->close();
+    }
     $connect->close();
 }
